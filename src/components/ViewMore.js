9ca56c7437971367dee2eaa4fb426643432d2793
Joy Renee Mugo <mugojoy24@gmail.com>
2024-05-06 09:00:12 +0300

    styled

--- src/components/ViewMore.js
+++ src/components/ViewMore.js
@@ -1,17 +1,1 @@
 
-import { NavLink } from "react-router-dom"
-
-function ViewMore() {
-    return(
-        <div>
-            <NavLink
-                to= "/viewmore"
-                className= "viewmore"
-            >
-                View More
-            </NavLink>
-        </div>
-    )
-}
-
-export default ViewMore
