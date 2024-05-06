52150dbcdea2947b9800f324650038f15bf55978
Joy Renee Mugo <mugojoy24@gmail.com>
2024-05-06 21:11:52 +0300

    post

--- src/components/ViewMore.js
+++ src/components/ViewMore.js
@@ -1,6 +1,6 @@
 
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import {  useNavigate, useParams } from "react-router-dom";
 
 function ViewMore() {
     const [bot, setBot] = useState({})
@@ -20,20 +20,53 @@ function ViewMore() {
     //     return <h1>Loading...</h1>;
     // };
 
+    const nav = useNavigate()
+
+    function goBack() {
+        nav("/")
+    }
+
+    function handleClick(bot) {
+        fetch("https://json-server-bots.onrender.com/bots/army", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify(bot)
+          })
+          .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            
+            nav("/")
+        })
+        console.log("clicked")
+    }
+    
+
     return(
         <>
-
+       
             
                 <div className="card " style={{width: "300px"}} >
-                    <img src={bot.avatar_url} class="card-img-top" alt={bot.name} />
+                    <img src={bot.avatar_url} className="card-img-top" alt={bot.name} />
                     <div className="card-body">
-                        <p className="card-text" style={{fontWeight: "bold"}} >
+                        <div className="card-text" style={{fontWeight: "bold"}} >
                             <div>Name: {bot.name}</div>
                             <div>Health: {bot.health}</div>
                             <div>Damage: {bot.damage}</div>
                             <div>Armor: {bot.armor}</div>
                             <div>Bot_class: {bot.bot_class}</div>
-                        </p>
+                            <br></br>
+                            <div>
+                                <button onClick={() =>handleClick(bot)}>Enlist</button>
+                            </div>
+                            <br></br>
+                            <div>
+                            <button onClick={goBack}>Go Back</button>
+                            </div>
+                        
+                        </div>
                     </div>
                 </div>
             
